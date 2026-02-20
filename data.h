d8a7d128f82856ebaf79f94fff4ca6b55c2093ed
haydenhunter096-png <haydenhunter096@gmail.com>
2026-02-20 01:21:29 +0000

    Update Address and Date classes

--- data.h
+++ data.h
@@ -5,14 +5,17 @@
 #define DATE_H_EXISTS
 
 class Date{
-  private:
+private:
     int month;
     int day;
     int year;
+
   public:
     Date();
+    Date(std::string dateString);
     void init(std::string dateString);
     void printDate();
+    std::string getDateString();
 }; // end class Date
 
 
